2b42cb4900dcff664b30fb94e79e75234512c42e
Oscar Megía López <pfgytk@gmail.com>
2020-02-20 11:42:00 +0100

    Modificado el Google Maps para que saque Alpe Formación y ajustados margenes en Página 1 y Home

--- views/site/_index.php
+++ views/site/_index.php
@@ -1,8 +1,8 @@
 <div class="col-lg-6">
     <div class="thumbnail">
         <div class="backgris miAlto1">
-            <h2><?= $noticias[$i]["Titulo"] ?></h2>
+            <h2 style="margin-left: 10px;"><br /><?= $noticias[$i]["Titulo"] ?><br /><br /></h2>
         </div>
-        <p><?= $noticias[$i]["Texto"] ?></p>
+        <p style="margin: 10px;"><?= $noticias[$i]["Texto"] ?></p>
     </div>
 </div>
--- views/site/pagina1.php
+++ views/site/pagina1.php
@@ -9,7 +9,7 @@ echo $this->render("_modal");
 <div style="text-align: center;">
     <?= Html::img("@web/imgs/viviendas-venta-Alcazares-consiguen-venderla_EDIIMA20200217_0114_20.jpg") ?>
 
-    <div style="width: 50%; margin: 0 auto;">
+    <div style="width: 50%; margin: 0 auto; margin-top: 25px">
         <div class="thumbnail">
             <div class="backgris miAlto1">
                 <h2><?= $noticiaAleatoria["Titulo"] ?></h2>
